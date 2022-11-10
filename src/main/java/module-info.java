1cfb2d1612f2ff4a488feeb69311f3853f73040d
Jerry Han <jerry000@hotmail.ca>
2022-11-10 14:47:46 -0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module ca.bcit.comp2522.termproject.catnapped {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ca.bcit.comp2522.termproject.catnapped to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.catnapped;
+}
