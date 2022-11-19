da3a4ace9b811bc51aea216ff4854cc719ec5bca
Bryan Tran <bryant.business@hotmail.com>
2022-11-18 17:30:00 -0800

    Comment out validatorfx because unknown error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,11 +5,12 @@ module ca.bcit.comp2522.termproject.catnapped {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires validatorfx;
+//    requires validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires net.synedra.validatorfx;
 
     opens ca.bcit.comp2522.termproject.catnapped to javafx.fxml;
     exports ca.bcit.comp2522.termproject.catnapped;
