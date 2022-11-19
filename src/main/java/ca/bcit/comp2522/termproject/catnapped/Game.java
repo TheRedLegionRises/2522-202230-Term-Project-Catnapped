5fe8d28e4d459cf3b75f13dfcde6116fd1969722
Bryan Tran <bryant.business@hotmail.com>
2022-11-18 21:21:51 -0800

    Add back old code from branch issue

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/Game.java
@@ -1,13 +1,47 @@
 package ca.bcit.comp2522.termproject.catnapped;
 
-public class Game {
+public class Game implements Runnable {
+
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int MAX_FPS = 120;
+
     public Game() {
+
         System.out.println("Game class works!");
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
 
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / MAX_FPS;
+        int frames = 0;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+        long lastCheck = System.currentTimeMillis();
+
+        while (true) {
+            now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
     }
 }
