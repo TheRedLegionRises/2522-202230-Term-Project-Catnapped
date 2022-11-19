fab6bc077c4e31af738523bf975ee6e1a2f1d7eb
Bryan Tran <bryant.business@hotmail.com>
2022-11-18 19:04:43 -0800

    Create a simple game loop with frame counter with threading

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/Game.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/Game.java
@@ -1,13 +1,50 @@
 package ca.bcit.comp2522.termproject.catnapped;
 
-public class Game {
+public class Game implements Runnable{
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int MAX_FPS = 120;
+
     public Game() {
         System.out.println("Game class works!");
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+
+    }
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / MAX_FPS;
+        long lastFrame = System.nanoTime();
+        long now = System.nanoTime();
+
+        int frames = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        while (true) {
+
+            now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+            }
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
 
     }
 }
