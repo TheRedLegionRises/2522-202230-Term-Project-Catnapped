7c56494b3f787e564100ad2efcc77891e670c585
Jerry Han <jerry000@hotmail.ca>
2022-11-20 12:59:52 -0800

    Added setMoving and setDirection methods from Bryan's GamePanel

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/GamePanel.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/GamePanel.java
@@ -7,55 +7,26 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static ca.bcit.comp2522.termproject.catnapped.Constants.PlayerAttributes.IDLE;
+
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
+    private int playerAction = IDLE;
+    private int playerDir = -1;
+    private boolean moving = false;
 
-    //    Moved to Player Class
-//    private BufferedImage img, subImg;
-
-//    private BufferedImage[] idleAnimation, runAnimation, deathAnimation
-//            ,jumpAnimation, attackAnimation, takeDamageAnimation;
-//    private int animationTick, animationIndex, animationSpeed = 15;
     private Game game;
     public GamePanel(Game game) {
         this.game = game;
 
         mouseInputs = new MouseInputs(this);
-//  Moved to Player Class
-//        importImg();
-//        loadAnimations();
+
         setPanelSize();
 
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
-//    Moved to player class
-//    private void loadAnimations() {
-//
-//        idleAnimation = new BufferedImage[6]; // If 5 - get an error - care
-//
-//        for(int i = 0; i < idleAnimation.length; i++)
-//            idleAnimation[i] = img.getSubimage(0 , i*16, 32 , 16); // Cat is 16 pixels tall and 32 pixels fat
-//
-//    }
-
-//    private void importImg() {
-//        InputStream is = getClass().getResourceAsStream("/images/King_Mewrthur_Idle.png"); // - 1 slash to reach images
-//
-//        try {img = ImageIO.read(is);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        finally {
-//            try{
-//                is.close();
-//            }
-//            catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 
     private void setPanelSize() {
         Dimension size = new Dimension(1280, 800); // Good arcade game size
@@ -66,24 +37,14 @@ public class GamePanel extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         game.render(g);
-
-//        Moved to Player class
-//        updateAnimationThread();
-        
-//        subImg = img.getSubimage(0 , 0*16, 32 , 16);  To get images within an image
-//        g.drawImage(idleAnimation[animationIndex],0 , 0, 160, 80,null);
-
     }
 
-//    private void updateAnimationThread() {
-//
-//        animationTick++;
-//        if(animationTick >= animationSpeed) {
-//            animationTick = 0;
-//            animationIndex++;
-//            if(animationIndex >= idleAnimation.length)
-//                animationIndex = 0;
-//        }
-//
-//    }
+    public void setDirection(int direction) {
+        this.playerDir = direction;
+        moving = true;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
 }
