6035d82e7e350ceec57e44dbca06462e9a71f67e
Jerry Han <jerry000@hotmail.ca>
2022-11-20 14:42:49 -0800

    Implemented Diagonal movement for player

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/KeyboardInputs.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/KeyboardInputs.java
@@ -21,10 +21,16 @@ public class KeyboardInputs implements KeyListener {
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
+                gamePanel.getGame().getPlayer().setMoveUp(false);
+                break;
             case KeyEvent.VK_A:
+                gamePanel.getGame().getPlayer().setMoveLeft(false);
+                break;
             case KeyEvent.VK_S:
+                gamePanel.getGame().getPlayer().setMoveDown(false);
+                break;
             case KeyEvent.VK_D:
-                gamePanel.getGame().getPlayer().setMoving(false);
+                gamePanel.getGame().getPlayer().setMoveRight(false);
                 break;
         }
     }
@@ -33,19 +39,19 @@ public class KeyboardInputs implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-                gamePanel.getGame().getPlayer().setDirection(UP);
+                gamePanel.getGame().getPlayer().setMoveUp(true);
                 System.out.println("W");
                 break;
             case KeyEvent.VK_A:
-                gamePanel.getGame().getPlayer().setDirection(LEFT);
+                gamePanel.getGame().getPlayer().setMoveLeft(true);
                 System.out.println("A");
                 break;
             case KeyEvent.VK_S:
-                gamePanel.getGame().getPlayer().setDirection(DOWN);
+                gamePanel.getGame().getPlayer().setMoveDown(true);
                 System.out.println("S");
                 break;
             case KeyEvent.VK_D:
-                gamePanel.getGame().getPlayer().setDirection(RIGHT);
+                gamePanel.getGame().getPlayer().setMoveRight(true);
                 System.out.println("D");
                 break;
         }
