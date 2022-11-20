6375fb411a2ef30767981e715072472fb772ea66
Bryan Tran <bryant.business@hotmail.com>
2022-11-20 11:30:01 -0800

    Implement a simple constructor for Player Class

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/Player.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/Player.java
@@ -1,4 +1,11 @@
 package ca.bcit.comp2522.termproject.catnapped;
 
 public class Player extends Actor{
+    private static final int maxHealth = 100;
+
+
+
+    public Player() {
+        super(maxHealth);
+    }
 }
