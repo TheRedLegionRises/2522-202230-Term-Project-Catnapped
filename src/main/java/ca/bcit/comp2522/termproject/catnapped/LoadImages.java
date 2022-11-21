da623b07c7278a25cea090cc62fea5c2917139bc
Jerry Han <jerry000@hotmail.ca>
2022-11-20 17:35:48 -0800

    Created separate class for loading images so it can be used universally

---
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/LoadImages.java
@@ -0,0 +1,32 @@
+package ca.bcit.comp2522.termproject.catnapped;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LoadImages {
+    public static final String PLAYER_RUNNING = "images/King_Mewrthur_Run.png";
+
+    public static BufferedImage GetImage(String filePath) {
+        BufferedImage img = null;
+        InputStream is = LoadImages.class.getResourceAsStream("/" + filePath);
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try{
+                is.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return img;
+    }
+
+
+}
