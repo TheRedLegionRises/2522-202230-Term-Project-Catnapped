01afbdf350aa0bdeddd1cb61f0a3a993d595abe4
Jerry Han <jerry000@hotmail.ca>
2022-11-22 19:18:17 -0800

    Added new method to display a level from a designated level picture

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/LoadImages.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/LoadImages.java
@@ -1,11 +1,14 @@
 package ca.bcit.comp2522.termproject.catnapped;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class LoadImages {
+    private static final String terrainImagesURL = "/images/Terrain.png";
+    private static final String testLevel = "/images/level_one_data.png";
 
     public static BufferedImage GetImage(String filePath) {
         BufferedImage img = null;
@@ -25,6 +28,25 @@ public class LoadImages {
             }
         }
         return img;
+    }
+
+    public static int[][] GetLevelImages() {
+        int[][] levelImages = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+        BufferedImage levelImg = GetImage(testLevel);
+
+        for (int j = 0; j < levelImg.getHeight(); j++) {
+            for (int i = 0; i < levelImg.getWidth(); i++) {
+                Color color = new Color(levelImg.getRGB(i, j));
+                int value = color.getRed();
+
+                if (value >= 48) {
+                    value = 0;
+                }
+                levelImages[j][i] = value;
+            }
+        }
+
+        return levelImages;
     }
 
 
