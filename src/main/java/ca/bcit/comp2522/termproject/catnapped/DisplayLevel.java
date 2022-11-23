a339e743e1f6e2b78360816e845c8bb816247ea3
Jerry Han <jerry000@hotmail.ca>
2022-11-22 19:17:45 -0800

    Added new method to display a level from a designated level picture

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/DisplayLevel.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/DisplayLevel.java
@@ -7,10 +7,12 @@ public class DisplayLevel {
     private Game game;
     private static final String terrainImagesURL = "/images/Terrain.png";
     private BufferedImage[] levelImages;
+    private LevelInfo level1;
 
     public DisplayLevel(Game game) {
         this.game = game;
         loadAllSprites();
+        level1 = new LevelInfo(LoadImages.GetLevelImages());
     }
 
     private void loadAllSprites() {
@@ -19,17 +21,24 @@ public class DisplayLevel {
         levelImages = new BufferedImage[247];
 
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 8; j++) {
-                int currentIndex = i * 8 + j;
+            for (int j = 0; j < 12; j++) {
+                int currentIndex = i * 12 + j;
                 levelImages[currentIndex] = img.getSubimage(j * 32, i * 32, 32, 32);
             }
         }
     }
 
     public void drawLevel(Graphics g) {
-        g.drawImage(levelImages[0], 10, 10, null);
-        g.drawImage(levelImages[1], 42, 10, null);
-        g.drawImage(levelImages[2], 74, 10, null);
+
+        for (int j = 0; j < Game.TILES_IN_HEIGHT; j++) {
+            for (int i = 0; i < Game.TILES_IN_WIDTH; i++) {
+                int index = level1.getSpriteIndex(i, j);
+                g.drawImage(levelImages[index], i * 32, j * 32, 32, 32, null);
+            }
+        }
+//        g.drawImage(levelImages[0], 10, 10, null);
+//        g.drawImage(levelImages[1], 42, 10, null);
+//        g.drawImage(levelImages[2], 74, 10, null);
 
     }
 
