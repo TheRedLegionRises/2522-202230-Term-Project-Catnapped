a887c9e770b82370edd14967dfba4d3c48e21b47
Jerry Han <jerry000@hotmail.ca>
2022-11-22 18:12:29 -0800

    Adjusted code for DisplayLevel to fit new Terrain file (has no blank spaces)

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/DisplayLevel.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/DisplayLevel.java
@@ -5,32 +5,32 @@ import java.awt.image.BufferedImage;
 
 public class DisplayLevel {
     private Game game;
-    private final String levelURL = "/images/Terrain.png";
+    private static final String terrainImagesURL = "/images/Terrain.png";
     private BufferedImage[] levelImages;
-    private BufferedImage levelImage;
 
     public DisplayLevel(Game game) {
         this.game = game;
-//        levelImage = LoadImages.GetImage(levelURL);
         loadAllSprites();
     }
 
     private void loadAllSprites() {
-        BufferedImage img = LoadImages.GetImage(levelURL);
+        BufferedImage img = LoadImages.GetImage(terrainImagesURL);
 //        System.out.println("Image Height: " + img.getHeight() + " | Image Width: " + img.getWidth());
         levelImages = new BufferedImage[247];
 
-        for (int i = 0; i < 13; i++) {
-            for (int j = 0; j < 19; j++) {
-                int currentIndex = i * 19 + j;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 8; j++) {
+                int currentIndex = i * 8 + j;
                 levelImages[currentIndex] = img.getSubimage(j * 32, i * 32, 32, 32);
             }
         }
     }
 
-    //Has Blank Spaces - 1 sprite worth along the outside
     public void drawLevel(Graphics g) {
-        g.drawImage(levelImages[20], 10, 10, null);
+        g.drawImage(levelImages[0], 10, 10, null);
+        g.drawImage(levelImages[1], 42, 10, null);
+        g.drawImage(levelImages[2], 74, 10, null);
+
     }
 
     public void update() {
