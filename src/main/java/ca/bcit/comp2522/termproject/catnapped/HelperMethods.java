9e53bae8cec980cecc70a6d127b56d2f97b8799f
Jerry Han <jerry000@hotmail.ca>
2022-11-24 12:29:03 -0800

    Created a class full of helper methods

---
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/HelperMethods.java
@@ -0,0 +1,40 @@
+package ca.bcit.comp2522.termproject.catnapped;
+
+public class HelperMethods {
+    public static boolean collisionDetection(float xCoordinate, float yCoordinate, int width, int height,
+                                             int[][] levelInfo) {
+
+        width /= 3;
+        xCoordinate += 40;
+
+        if(!isSolidTile(xCoordinate, yCoordinate, levelInfo)) {
+            if(!isSolidTile(xCoordinate + width, yCoordinate + height, levelInfo)) {
+                if(!isSolidTile(xCoordinate + width, yCoordinate, levelInfo)) {
+                    if(!isSolidTile(xCoordinate, yCoordinate + height, levelInfo))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isSolidTile(float x, float y, int[][] levelInfo) {
+        if (x < 0 || x >= Game.GAME_WINDOW_WIDTH) {
+            return true;
+        }
+        if (y < 0 || y >= Game.GAME_WINDOW_HEIGHT) {
+            return true;
+        }
+
+        float xIndex = x / Game.DEFAULT_TILE_SIZE;
+        float yIndex = y / Game.DEFAULT_TILE_SIZE;
+
+        int value = levelInfo[(int) yIndex][(int) xIndex];
+
+        if(value >= 48 || value < 0 || value != 11) {
+            return true;
+        }
+        return false;
+
+    }
+}
