f3e4598b1e9f14061417af4a127b7edf341518a9
Jerry Han <jerry000@hotmail.ca>
2022-11-24 11:29:41 -0800

    Changed Private variables to Protected Created a hitbox around Player (visual only)

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/Actor.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/Actor.java
@@ -1,13 +1,16 @@
 package ca.bcit.comp2522.termproject.catnapped;
 
+import java.awt.*;
+
 /**
  * Abstract Class Entity.
  * @author jerry
  * @version 2022
  */
 public abstract class Actor {
-    private final float x, y;
-    private final int currentHealth, maxHealth, height, width;
+    protected float x, y;
+    protected final int currentHealth, maxHealth, height, width;
+    private Rectangle playerHitbox;
 
 
     public Actor (float newXCoordinate, float newYCoordinate, int newMaxHealth, int newHeight, int newWidth) {
@@ -17,6 +20,25 @@ public abstract class Actor {
         this.currentHealth = newMaxHealth;
         this.height = newHeight;
         this.width = newWidth;
+
+        playerHitbox = new Rectangle((int) x, (int) y, width, height);
+    }
+
+    public void updateHitbox() {
+        //Empty white space to left because of the way sprite was created so we shift hitbox right
+        playerHitbox.x = (int) x + 40;
+        playerHitbox.y = (int) y;
+    }
+
+    //Could be protected
+    public Rectangle getPlayerHitbox() {
+        return playerHitbox;
+    }
+
+    public void drawPlayerHitbox(Graphics g) {
+        g.setColor(Color.PINK);
+        //Divided hitbox width by 3 to be more lenient on player (less raging)
+        g.drawRect(playerHitbox.x, playerHitbox.y, playerHitbox.width / 3, playerHitbox.height);
     }
 
     public float getXCoordinate() {
