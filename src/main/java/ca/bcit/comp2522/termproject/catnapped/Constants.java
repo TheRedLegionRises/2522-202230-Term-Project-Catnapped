9c62e621593e6ba46728d68110c540449811a748
Jerry Han <jerry000@hotmail.ca>
2022-11-26 17:06:28 -0800

    Added new constants for Enemies

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/Constants.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/Constants.java
@@ -1,6 +1,35 @@
 package ca.bcit.comp2522.termproject.catnapped;
 
 public class Constants {
+    public static class EnemyConstants {
+        public static final int IDLE = 0;
+        public static final int RUNNING = 1;
+        public static final int TAKE_DAMAGE = 2;
+        public static final int ATTACK = 3;
+        public static final int DEATH = 4;
+
+        public static final int ENEMY_WIDTH = 32;
+        public static final int ENEMY_HEIGHT = 32;
+
+        public static int GetEnemyAttribute(int enemy_action) {
+
+            switch (enemy_action) {
+                case RUNNING:
+                    return 6;
+                case IDLE:
+                    return 11;
+                case TAKE_DAMAGE:
+                    return 2;
+                case ATTACK:
+                    return 5;
+                case DEATH:
+                    return 4;
+                default:
+                    return 1;
+
+            }
+
+        }    }
 
     public static class PlayerAttributes {
 
@@ -10,7 +39,6 @@ public class Constants {
         public static final int TAKE_DAMAGE = 3;
         public static final int ATTACK = 4;
         public static final int DEATH = 5;
-        public static final int FALLING = 6;
 
 
         public static int GetPlayerAttribute(int player_action) {
