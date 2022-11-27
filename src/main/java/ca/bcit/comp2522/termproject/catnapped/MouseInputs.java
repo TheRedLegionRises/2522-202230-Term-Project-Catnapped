8acaa40c36a12f9fcbb98225f32f0fc89c61cdb6
Bryan Tran <bryant.business@hotmail.com>
2022-11-26 19:19:17 -0800

    Implement mouse inputs for Menu state / interface

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/MouseInputs.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/MouseInputs.java
@@ -16,27 +16,44 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
     @Override
     public void mouseClicked(MouseEvent e) {
 //        System.out.println("Mouse Clicked");
-        switch(Gamestate.state) {
-            case MENU:
-                gamePanel.getGame().getMenu().mouseClicked(e);
-
-                break;
+        switch (Gamestate.state) {
             case INGAME:
                 gamePanel.getGame().getInGame().mouseClicked(e);
                 break;
             default:
                 break;
-    }
+        }
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
 
+        switch (Gamestate.state) {
+            case MENU:
+                gamePanel.getGame().getMenu().mousePressed(e);
+
+                break;
+            case INGAME:
+                gamePanel.getGame().getInGame().mousePressed(e);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU:
+                gamePanel.getGame().getMenu().mouseReleased(e);
 
+                break;
+            case INGAME:
+                gamePanel.getGame().getInGame().mouseReleased(e);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -56,6 +73,18 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
     @Override
     public void mouseMoved(MouseEvent e) {
+        switch (Gamestate.state) {
+            case MENU:
+                gamePanel.getGame().getMenu().mouseMoved(e);
 
+                break;
+            case INGAME:
+                gamePanel.getGame().getInGame().mouseMoved(e);
+                break;
+            default:
+                break;
+
+        }
     }
+
 }
