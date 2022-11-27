1272a4cd0f395439019e544425c954249bbc5314
Jerry Han <jerry000@hotmail.ca>
2022-11-26 18:35:08 -0800

    Added new method to load all enemy images for animations Added new variable for upate level with 3 enemies

--- src/main/java/ca/bcit/comp2522/termproject/catnapped/LoadImages.java
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/LoadImages.java
@@ -5,10 +5,14 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
+import static ca.bcit.comp2522.termproject.catnapped.Constants.EnemyConstants.*;
 
 public class LoadImages {
     private static final String terrainImagesURL = "/images/Terrain.png";
     private static final String testLevel = "/images/level_one_data.png";
+    private static final String testLevel1 = "/images/test_level.png";
 
     public static BufferedImage GetImage(String filePath) {
         BufferedImage img = null;
@@ -30,11 +34,31 @@ public class LoadImages {
         return img;
     }
 
-    public static int[][] GetLevelImages() {
-        int[][] levelImages = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
-        BufferedImage levelImg = GetImage(testLevel);
+    public static ArrayList<Enemy> GetEnemies() {
+        BufferedImage levelImg = GetImage(testLevel1);
+        ArrayList<Enemy> enemyList = new ArrayList<>();
 
         for (int j = 0; j < levelImg.getHeight(); j++) {
+            for (int i = 0; i < levelImg.getWidth(); i++) {
+                Color color = new Color(levelImg.getRGB(i, j));
+                int value = color.getGreen();
+
+                if (value == ENEMY_IDENTIFIER) {
+                    enemyList.add(new Enemy(i * Game.DEFAULT_TILE_SIZE,
+                            j * Game.DEFAULT_TILE_SIZE, 1, ENEMY_HEIGHT, ENEMY_WIDTH));
+                }
+            }
+        }
+        return enemyList;
+
+    }
+
+    public static int[][] GetLevelImages() {
+        int[][] levelImages = new int[Game.TILES_IN_HEIGHT][Game.TILES_IN_WIDTH];
+        BufferedImage levelImg = GetImage(testLevel1);
+
+        for (int j = 0; j < levelImg.getHeight(); j++) {
+//            System.out.print("J: " + j);
             for (int i = 0; i < levelImg.getWidth(); i++) {
                 Color color = new Color(levelImg.getRGB(i, j));
                 int value = color.getRed();
@@ -45,7 +69,6 @@ public class LoadImages {
                 levelImages[j][i] = value;
             }
         }
-
         return levelImages;
     }
 
