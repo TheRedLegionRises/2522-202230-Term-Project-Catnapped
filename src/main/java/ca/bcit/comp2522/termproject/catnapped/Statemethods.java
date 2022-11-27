d74f650f470940eb30088aa5747036e8c1ad91b5
Bryan Tran <bryant.business@hotmail.com>
2022-11-26 18:12:21 -0800

    Implement an interface for methods when in different states

---
+++ src/main/java/ca/bcit/comp2522/termproject/catnapped/Statemethods.java
@@ -0,0 +1,25 @@
+package ca.bcit.comp2522.termproject.catnapped;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public interface Statemethods {
+
+    public void update();
+
+    public void draw(Graphics g);
+
+    public void keyPressed(KeyEvent e);
+
+    public void keyReleased(KeyEvent e);
+
+    public void mouseClicked(MouseEvent e);
+
+    public void mousePressed(MouseEvent e);
+
+    public void mouseReleased(MouseEvent e);
+
+    public void mouseMoved(MouseEvent e);
+
+}
